1411a89a90365230561ddb00e6a5499f5f995ea9
Praharsh Paravastu <paravastupraharsh99@gmail.com>
2020-10-08 15:37:27 +0530

    Added the professional experience section

---
+++ src/components/professional-experience/professional-experience.component.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+
+import ContentInput from '../content-input/content-input.component';
+
+import './professional-experience.style.css';
+
+const ProfessionalExp = () => {
+  const style1 = {
+    textTransform: 'uppercase',
+    fontSize: '18px',
+    fontWeight: 'bold',
+    color: '#000',
+  };
+
+  const style2 = {
+    fontSize: '18px',
+    color: '#bb3d76',
+    fontWeight: '700',
+  };
+
+  const style3 = {
+    textTransform: 'uppercase',
+    fontSize: '14px',
+    fontWeight: '600',
+    padding: '2px 5px',
+  };
+
+  const style4 = {
+    fontSize: '14px',
+    fontWeight: '400',
+    padding: '3px 5px',
+  };
+
+  return (
+    <div className="professionalExperience">
+      <ContentInput
+        placeholder="Professional Experience"
+        content="Professional Experience"
+        style={style1}
+      />
+
+      <div className="professionalExperience__sub">
+        <div className="professionalExperience__section">
+          {/* TODO: Add the interaction buttons to each section */}
+          <ContentInput placeholder="Position" style={style2} />
+
+          <ContentInput
+            placeholder="Company Name"
+            placeholderBold
+            style={style3}
+          />
+
+          <ContentInput
+            placeholder="In this text field your can describe your duties. Try to focus on accomplishments that serve as concrete examples to the type of problems you can solve to your future employer."
+            style={style4}
+            placeholderSemiBold
+          />
+        </div>
+
+        <div className="professionalExperience__section">
+          {/* TODO: Add the interaction buttons to each section */}
+          <ContentInput placeholder="Position" style={style2} />
+
+          <ContentInput
+            placeholder="Company Name"
+            placeholderBold
+            style={style3}
+          />
+
+          <ContentInput
+            placeholder="In this text field your can describe your duties. Try to focus on accomplishments that serve as concrete examples to the type of problems you can solve to your future employer."
+            style={style4}
+            placeholderSemiBold
+          />
+        </div>
+        <div className="professionalExperience__section">
+          {/* TODO: Add the interaction buttons to each section */}
+          <ContentInput placeholder="Position" style={style2} />
+
+          <ContentInput
+            placeholder="Company Name"
+            placeholderBold
+            style={style3}
+          />
+
+          <ContentInput
+            placeholder="In this text field your can describe your duties. Try to focus on accomplishments that serve as concrete examples to the type of problems you can solve to your future employer."
+            style={style4}
+            placeholderSemiBold
+          />
+        </div>
+        <div className="professionalExperience__section">
+          {/* TODO: Add the interaction buttons to each section */}
+          <ContentInput placeholder="Position" style={style2} />
+
+          <ContentInput
+            placeholder="Company Name"
+            placeholderBold
+            style={style3}
+          />
+
+          <ContentInput
+            placeholder="In this text field your can describe your duties. Try to focus on accomplishments that serve as concrete examples to the type of problems you can solve to your future employer."
+            style={style4}
+            placeholderSemiBold
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfessionalExp;
--- src/components/right-bar/right-bar.component.jsx
+++ src/components/right-bar/right-bar.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import Education from '../education/education.component';
+import ProfessionalExp from '../professional-experience/professional-experience.component';
 
 import './right-bar.styles.css';
 
 const RightBar = () => {
   return (
     <div className="rightBar">
+      <ProfessionalExp />
+
       <Education />
     </div>
   );
